864074d2f830df5b8039c943bd8524f5f24c7f8c
IrinaBreus <irabreus@gmail.com>
2022-09-20 19:35:48 +0300

    add Button P Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, P, Tag } from "../components";
 
 
 export default function Home(): JSX.Element {
   return (
     <div >
       <Htag tag='h1'>Курсы по Photoshop</Htag>
-      <Button appearance="primary">Кнопка</Button>
-      <Button appearance="ghost">Кнопка</Button>
+      <Button appearance="primary" arrow="right">Кнопка</Button>
+      <Button appearance="ghost" arrow="down">Кнопка</Button>
+      <P size="l">sdfslkjfslkdfsldkf</P>
+      <P size="m">sdfslkjfslkdfsldkf</P>
+      <P size="s">sdfslkjfslkdfsldkf</P>
+      <Tag size="s">ghost</Tag>
+      <Tag size="m" color="green">green</Tag>
+      <Tag size="s" color="primary">primary</Tag>
+      <Tag size="m" color="red">ghost</Tag>
     </div>
   );
 }
