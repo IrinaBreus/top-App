95e25f41ae462e379348f629fa2d79c68e009416
IrinaBreus <irabreus@gmail.com>
2022-09-29 11:55:38 +0300

    add footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import { FooterProps } from "./Footer.props";
+import styles from "./Footer.module.css";
+import cn from "classnames";
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}
+            >
+            <div >OwlTop © 2020 - {new Date().getFullYear()} Все права защищены</div>
+            <a href="#">Пользовательское соглашение</a>
+            <a href="#">Политика конфиденциальности</a>
+        </footer>
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <title>MyTop - наш лучший топ</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />rel
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;500;700&display=swap" />
       </Head>
     <Component {...pageProps} />
   </>;
