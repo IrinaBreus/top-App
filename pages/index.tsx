122945c6dcab3fcc1a7e3d0067a149da03a0f2f9
IrinaBreus <irabreus@gmail.com>
2022-10-05 12:56:11 +0300

    add Paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,52 @@
+import axios from 'axios';
+import { GetStaticProps } from "next";
+import process from 'process';
 import React, { useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
+import { MenuItem } from '../interfaces/menu.interface';
 import { withLayout } from "../layout/Layout";
 
 
 
-function Home(): JSX.Element {
-  const [rating, setRating] = useState<number>(0);
-  return (
-    < >
-      <Htag tag='h1'>Заголовок</Htag>
-      <Button appearance="primary" arrow="right" >Кнопка</Button>
-      <Button appearance="ghost" arrow="down">Кнопка</Button>
-      <P size="l">Большой</P>
-      <P size="m">основной</P>
-      <P size="s">маленький</P>
-      <Tag size="s">ghost</Tag>
-      <Tag size="m" color="green">green</Tag>
-      <Tag size="s" color="primary">primary</Tag>
-      <Tag size="m" color="red">ghost</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
-    </>
-  );
+function Home({menu, firstCategory}: HomeProps): JSX.Element {
+	const [rating, setRating] = useState<number>(0);
+	return (
+	<>
+		<Htag tag='h1'>Заголовок</Htag>
+		<Button appearance="primary" arrow="right" >Кнопка</Button>
+		<Button appearance="ghost" arrow="down">Кнопка</Button>
+		<P size="l">Большой</P>
+		<P size="m">основной</P>
+		<P size="s">маленький</P>
+		<Tag size="s">ghost</Tag>
+		<Tag size="m" color="green">green</Tag>
+		<Tag size="s" color="primary">primary</Tag>
+		<Tag size="m" color="red">ghost</Tag>
+		<Rating rating={rating} isEditable setRating={setRating}/>
+		<ul>
+			{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+		</ul>
+	</>
+	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+	const firstCategory = 0;
+
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown>{
+	menu: MenuItem[],
+	firstCategory: number
+}
